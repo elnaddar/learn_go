0180dd852b9c9afffb1b0e726049475e00fa36c7
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-11-08 17:11:11 +0200

    feat: make NewBlogPost function parses meta data and body well.

--- q_read_files/blogposts/blogposts.go
+++ q_read_files/blogposts/blogposts.go
@@ -1,12 +1,30 @@
 package blogposts
 
 import (
-	"testing/fstest"
+	"io/fs"
 )
 
-type Post struct {
+func NewPostsFromFS(filesystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(filesystem, ".")
+	if err != nil {
+		return nil, err
+	}
+	var posts []Post
+	for _, f := range dir {
+		post, err := getPost(filesystem, f.Name())
+		if err != nil {
+			return nil, err //todo: needs clarification, should we totally fail if one file fails? or just ignore ?
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
 }
 
-func NewPostsFromFS(filesystem fstest.MapFS) []Post {
-	return []Post{}
+func getPost(filesystem fs.FS, f string) (Post, error) {
+	postFile, err := filesystem.Open(f)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile)
 }
---
+++ q_read_files/blogposts/post.go
@@ -0,0 +1,50 @@
+package blogposts
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type Post struct {
+	Title       string
+	Description string
+	Tags        []string
+	Body        string
+}
+
+const (
+	titleSeparator       = "Title: "
+	descriptionSeparator = "Description: "
+	tagsSeparator        = "Tags: "
+)
+
+func newPost(f io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(f)
+
+	readMetaLine := func(tagName string) string {
+		scanner.Scan()
+		return strings.TrimPrefix(scanner.Text(), tagName)
+	}
+
+	post := Post{
+		Title:       readMetaLine(titleSeparator),
+		Description: readMetaLine(descriptionSeparator),
+		Tags:        strings.Split(readMetaLine(tagsSeparator), ", "),
+		Body:        readBody(scanner),
+	}
+	return post, nil
+}
+
+func readBody(scanner *bufio.Scanner) string {
+	scanner.Scan() // ignore one line
+
+	buf := bytes.Buffer{}
+	for scanner.Scan() {
+		fmt.Fprintln(&buf, scanner.Text())
+	}
+
+	return strings.TrimSuffix(buf.String(), "\n")
+}
